2ca1405e9b4c321ef7f6c79356d65a35e1ec3233
danikachrunik@gmail.com Chrunik <danikachrunik@gmail.com>
2018-02-09 00:17:47 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -4,6 +4,24 @@ function getIt() {
   });
 }
 
+function frameIt() {
+  $('img').on('load', function(){
+  
+  });
+}
+
+function pressIt() {
+  $('img').on('load', function(){
+  
+  });
+}
+
+function submitIt() {
+  $('img').on('load', function(){
+  
+  });
+}
+
 $(document).ready(function(){
 
 // call functions here
